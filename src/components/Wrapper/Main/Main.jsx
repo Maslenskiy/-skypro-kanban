ec9de5bc128dd5fd7e68fbbda974c811585d0ad0
Yuriy Maslenskiy <maslenski2000@mail.ru>
2025-06-23 20:09:12 +0300

    сделал модальное окно пользователя, сделал имитацию загрузки данных используя хук useEffect, перенес данные карточек в массив обьекстов

--- src/components/Wrapper/Main/Main.jsx
+++ src/components/Wrapper/Main/Main.jsx
@@ -1,15 +1,21 @@
+
 import MainColumn from "./MainColumn/MainColumn";
-export default function Main() {
+export default function Main({cardList,status}) {
     return (
     <main className="main">
     <div className="container">
       <div className="main__block">
         <div className="main__content">
-        <MainColumn status={'Без статуса'} color={'_orange'} cardTheme="card__theme _orange" title='Web Design'/>
-        <MainColumn status={'Нужно сделать'}  color={'_green'} cardTheme="card__theme _green"  title='Research'/>
-        <MainColumn status={'В работе'}  color={'_orange'} cardTheme="card__theme _orange"  title='Web Design'/>
-        <MainColumn status={'Тестирование'} color={'_purplr'} cardTheme="card__theme _purple"  title='Copywriting'/>
-        <MainColumn status={'Готово'}  color={'_green'} cardTheme="card__theme _green"  title='Research'/>
+            {status.map((status) => {
+              const filteredCards = cardList.filter(card => card.status === status.title);
+              return (
+                <MainColumn 
+                  key={status.id} 
+                  status={status.title} 
+                  cardList={filteredCards} 
+                />
+              );
+            })}
         </div>
       </div>
     </div>
